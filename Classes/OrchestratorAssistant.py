5da2b0f63639eaa3f22709360c207668aad8c5ab
i-vt <99633800+i-vt@users.noreply.github.com>
2023-12-08 09:16:46 -0500

    Update OrchestratorAssistant.py

--- Classes/OrchestratorAssistant.py
+++ Classes/OrchestratorAssistant.py
@@ -1,1 +1,8 @@
 # Define a combination of classes & functionalities for an easier pull by OchestratorMain.py
+
+"""
+To do:
+Create a class
+Use threading to "record", argument for default variables
+Use threading to "play", argument for keyboard & mouse
+"""
