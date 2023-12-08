5a31f8d5cca1be159cac19ace87c20b3e233c443
i-vt <99633800+i-vt@users.noreply.github.com>
2023-12-08 00:57:06 -0500

    Update TrackKeyboard.py

--- Classes/TrackKeyboard.py
+++ Classes/TrackKeyboard.py
@@ -1,13 +1,25 @@
-from pynput.keyboard import Listener
+from pynput.keyboard import Key, Listener
 
-def on_keypress(key):
+def on_press(key):
     try:
-        # Print the key that was pressed
-        print(f'Key pressed: {key.char}')
-    except AttributeError:
-        # Handle special keys or key combinations
-        print(f'Special key pressed: {key}')
+        # Check if the key is a special key (like space, enter, etc.)
+        if isinstance(key, Key):
+            print(f"Special key {key} pressed")
+        else:
+            # Check if the key is a special character (not a letter or digit)
+            if not key.char.isalnum():
+                print(f"Special character {key.char} pressed")
+            else:
+                print(f"Alphanumeric key {key.char} pressed")
 
-# Create a listener that calls the on_keypress function for keyboard events
-with Listener(on_press=on_keypress) as listener:
+    except AttributeError:
+        print(f"Special key (without char) {key} pressed")
+
+def on_release(key):
+    # Stop listener
+    if key == Key.esc:
+        return False
+
+# Collect events until released
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
