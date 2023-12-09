b177265a5c77214061846ff4d3f8cd2be65988b9
i-vt <99633800+i-vt@users.noreply.github.com>
2023-12-09 16:08:30 -0500

    Update OrchestratorAssistant.py

--- Classes/OrchestratorAssistant.py
+++ Classes/OrchestratorAssistant.py
@@ -1,8 +1,32 @@
 # Define a combination of classes & functionalities for an easier pull by OchestratorMain.py
 
-"""
-To do:
-Create a class
-Use threading to "record", argument for default variables
-Use threading to "play", argument for keyboard & mouse
-"""
+import Track.TrackKeyboard as tk, Track.TrackMouse as tm, Track.TrackScreen as ts
+import Use.UseKeyboard as uk, Use.UseMouse as um
+
+import threading
+
+class OrchestratorAssistant:
+    
+    def __init__(self, configs: str = "default"):
+        self.configs = configs
+        """
+        to do: develop new profiles & different configs
+        """
+
+    def record(self):
+        keyboard =  = threading.thread(target=tk().record())
+        mouse  = threading.thread(target=tm().record())
+        screen  = threading.thread(target=ts().record())
+        
+        screen.start()
+        mouse.start()
+        keyboard.start()
+
+    def play(self, mouse, keyboard):
+        if mouse != "":
+            mouse_thread = threading.Thread(target=um.play_record(mouse))
+            mouse_thread.start()
+        if keyboard != "":
+            keyboard_thread = threading.thread(target=uk.play_record(keyboard))
+            keyboard_thread.start()
+        # A bit concerned about delay & file mismatch tbh
