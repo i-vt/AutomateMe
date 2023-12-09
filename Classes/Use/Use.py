7ee063d24a1a44f40639aa848da79432be68399e
i-vt <99633800+i-vt@users.noreply.github.com>
2023-12-09 13:56:25 -0500

    Update Use.py

--- Classes/Use/Use.py
+++ Classes/Use/Use.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import os
 class Use:
 
     def __init__(self, convert: bool = False):
@@ -21,9 +21,7 @@ class Use:
         return True
         
         
-        
-    
-    def time_difference(time1, time2) -> float:
+    def time_difference(self,time1, time2) -> float:
         """
         time_difference("20231208_000511_546018", "20231208_000411_546018")
         00:05:11, 00:04:11
@@ -37,13 +35,57 @@ class Use:
 
         if datetime1 > datetime2:
             datetime1, datetime2 = datetime2, datetime1
-
+        #print(datetime1, datetime2)
         delta = datetime2 - datetime1
 
         return delta.total_seconds()
+
+    def read_file(self, input_file: str="", delimiter: str="|") -> list:
+        """
+        input_file name:
+        /home/computer/Documents/Output/TrackKeyboard_20231208_021718_368217.txt
+        
+        File contents:
+        20231208_004930_177528|on_keypress|Key pressed: s
+        20231208_004930_227942|on_keypress|Key pressed: d
+        20231208_004930_279579|on_keypress|Key pressed: f
+        20231208_004931_434505|on_keypress|Key pressed: c
+        
+        Output of function:
+        [[0.550266, 'on_keypress', 'Key pressed: s'],
+        ....
+         [0.893151, 'on_keypress', 'Key pressed: c']]
+
+        """
+
+        if "" in [input_file, delimiter]: return []
+
+        _, filename = os.path.split(input_file)
+        start_time = filename.split(".")[0][-22:]
+
+        with open(input_file, "r") as file: lines = file.read().split("\n")[:-1]
+
+        list_return = []
+        for line in lines:
+            if line == [] or line.count(delimiter) == 0: continue
+
+            append_line = line.split(delimiter)
+            end_time = append_line[0]
+            append_line[0] = self.time_difference(append_line[0], start_time)
+            start_time = end_time
+            
+            list_return.append(append_line)
+        return list_return
+
+
+
+
+
+    #Make a converter for ML + converter to second time
         
 
-        
+#print(Use().read_file("/home/blender/Documents/updated_/Output/TrackKeyboard_20231208_004929_627262.txt"))
+
 #test = ["20231208_000411_546018","202_31208_000411_546018","2023208_0004111_546018"]
 #for i in test: print(Use.check_time_formatting(i))
 
