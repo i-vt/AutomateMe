e1a0876f38eac23dcdc83e97bac4b4f340649188
i-vt <99633800+i-vt@users.noreply.github.com>
2023-12-11 00:44:00 -0500

    Update TrackHelper.py

--- Classes/Track/TrackHelper.py
+++ Classes/Track/TrackHelper.py
@@ -3,22 +3,29 @@ import threading
 if __name__ != "__main__":
     from .TrackKeyboard import TrackKeyboard as tk
     from .TrackMouse import TrackMouse as tm
-    from .TrackScreen import TrackScreen as ts
+    from .TrackScreen import TrackScreen as tsc
+    from .TrackSystem import TrackSystem as tsy
 else:
     from TrackKeyboard import TrackKeyboard as tk
     from TrackMouse import TrackMouse as tm
-    from TrackScreen import TrackScreen as ts
+    from TrackScreen import TrackScreen as tsc
+    from TrackSystem import TrackSystem as tsy
 
 class TrackHelper:
     
     def __init__(self, configs: str = "default"):
         self.configs = configs
+        if configs == "default":
+            t_system = tsy()
+            t_system.basic_sysinfo()
+            t_system.hardware()
+            t_system.network()
         """
         to do: develop new profiles & different configs
         """
     def mouse(self):tm().record()
     def keyboard(self):tk().record()
-    def screen(self):ts().record()
+    def screen(self):tsc().record()
 
     def record(self):
         #Easier to do it this way so they do not self-initiate lol b/c of multiple parenthesis
